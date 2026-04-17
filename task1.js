7a936344791bd65715e02d82c872719108153480
ThizisRKP <editorsclubindia458@gmail.com>
2026-04-17 13:24:54 +0530

    task1 added

--- task1.js
+++ task1.js
@@ -1,1 +1,105 @@
-console.log("hello from task 1");
+//Task 1 :  Variable Update
+
+var val = 50;
+console.log(val);
+let num1 = val;
+var val = 100;
+console.log(val);
+
+//Task 2: Let Variable Math
+let marks = 80;
+marks += 10;
+console.log(marks);
+
+//Task 3: Const Value
+const prize = 500;
+const finalPrize = prize + 100;
+console.log(finalPrize);
+
+// Task 4: Printing Statements
+let greet = "welcome Team";
+let year = 2026;
+let statement = true;
+
+console.log(greet);
+console.log(year);
+console.log(statement);
+
+//Task 5: Datatype Check
+console.log(typeof "JavaScript");
+console.log(typeof 250);
+console.log(typeof false);
+
+
+// Task 6: Array Task
+const fruits = ["apple", "banana", "orange" , "mango" , "graps"];
+console.log(fruits);
+console.log("First fruit : "+fruits[0]);
+console.log("Last fruit : " + fruits[fruits.length-1]);
+
+
+// Task 7: Object Task
+const student = {
+    name : "Janani",
+    age : "22"
+};
+console.log(student.name);
+console.log(student.age);
+
+// Task 8: Arithmetic Operators
+console.log(20+10);
+console.log(50-25);
+console.log(5*5);
+console.log(100/4);
+console.log(20%3);
+
+//Task 9: Increment / Decrement
+let x = 5;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+
+//Task 10: Comparison Operators
+console.log(10 > 5);
+console.log(5 < 2);
+console.log(20 == "20");
+console.log(15 === "15");
+console.log(10 != 8);
+
+
+// Task 11: Logical Operators
+console.log(5>2 && 10>3);
+console.log(7<5 || 8>2);
+console.log(!(10>5));
+
+//Task 12: Ternary Operator
+
+let age = 18;
+(age >= 18) ? console.log("Eligible") : console.log("Not Eligible");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
